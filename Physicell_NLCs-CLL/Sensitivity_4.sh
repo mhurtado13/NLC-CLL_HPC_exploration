a6457b4e9b0e89f48fcc1d8be932d66ab10525d2
mhurtado13 <marcelo.hurt137@gmail.com>
2024-07-22 10:24:12 +0200

    Ready for sensitivity analysis

---
+++ Physicell_NLCs-CLL/Sensitivity_4.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=NLC-CLL-Sensitivity-node4
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=256G
+#SBATCH --partition=unlimitq
+#SBATCH --mail-type=FAIL,BEGIN,END
+#SBATCH --mail-user=marcelo.hurtado@inserm.fr
+#SBATCH -o /home/mhurtado/work/NLC-CLL_HPC_exploration/Physicell_NLCs-CLL/logs/Sensitivity/%x.o
+#SBATCH -e /home/mhurtado/work/NLC-CLL_HPC_exploration/Physicell_NLCs-CLL/logs/Sensitivity/%x.e
+
+#Define file of samples to run exploration
+FILE="data_output/Sensitivity_analysis/samples/Samples_3.csv"
+#Define the number of tasks running in parallel
+NUM_TASKS=32
+#Define the number replicates for bootstrapping 
+NUM_REPLICATES=10
+#Specify which node to use
+NODE=4
+
+python scripts/run_model.py $FILE $NUM_TASKS $NUM_REPLICATES $NODE
+
+#Before running this, run in the console: python param_sensitivity.py $NUM_SAMPLES $NUM_NODES
+#Remember for Sobol: N*(2D + 2)  simulations
+
+
+
+
+
+
