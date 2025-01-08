5d0d5fed6be8a9b3d8d550554a3eef6c9f47d0f7
mhurtado13 <marcelo.hurt137@gmail.com>
2025-01-08 01:08:58 +0100

    Update sampling from parameters selected for sensitivity analysis

--- Physicell_NLCs-CLL/scripts/src/sampling.py
+++ Physicell_NLCs-CLL/scripts/src/sampling.py
@@ -5,33 +5,25 @@ import numpy as np
 def sobol_sampling(nsamples):
     
     
-    ##number of inputs = 20
+    ##number of inputs = 14
 
-    default_values = {'uptake_rate_cancer': 1.0, 'secretion_rate_cancer':1.0, 'speed_cancer': 1.0, 'transformation_rate_cancer': 3e-5, 'relative_maximum_adhesion_distance_cancer': 0, 'cell_adhesion_affinity_cancer': 1,
-                      'speed_monocytes': 1.0, 'transformation_rate_monocytes': 95e-6, 'phagocytosis_rate_monocytes':25e-2, 'speed_macrophages': 1.0, 'phagocytosis_rate_macrophages':92e-2, 
-                      'attack_rate_macrophages': 5e-2, 'relative_maximum_adhesion_distance_macrophages': 0, 'cell_adhesion_affinity_macrophages': 1, 'speed_NLCs': 1.0, 'phagocytosis_rate_NLCs':4e-2, 
-                      'secretion_rate_apoptotic': 1.0, 'speed_apoptotic': 1.0, 'transformation_rate_apoptotic': 5e-05, 'secretion_rate_dead': 1.0}
+    default_values = {'cell_cell_repulsion_strength_cancer': 30, 'speed_cancer': 2, 'phagocytosis_rate_dead_monocytes': 25e-4,
+                      'transformation_rate_NLCs_monocytes': 3e-15, 'uptake_rate_cytokines_macrophages': 1,
+                      'attack_rate_cancer_macrophages': 5e-2, 'phagocytosis_rate_apoptotic_macrophages': 92e-4,
+                      'phagocytosis_rate_dead_macrophages': 92e-4, 'transformation_rate_NLCs_macrophages': 3e-16,
+                      'cell_cell_adhesion_strength_NLCs': 1, 'attachment_rate_NLCs': 0.001, 
+                      'phagocytosis_rate_dead_NLCs': 4e-4, 'speed_apoptotic': 2, 'transformation_rate_dead_apoptotic': 5e-5}
     
     
-    #Parameters left after exploration
-    '''
-    default_values = {'secretion_rate_cancer':1.0, 'speed_cancer': 1.0, 'transformation_rate_cancer': 3e-5, 'speed_monocytes': 1.0, 'transformation_rate_monocytes': 95e-6, 
-                      'relative_maximum_adhesion_distance_macrophages': 0, 'speed_NLCs': 1.0, 'speed_apoptotic': 1.0}
-    
-    'bounds': np.array([[0, 0, 3e-7, 0, 95e-7, 0, 0, 0],
-                        [3, 3, 3e-3, 3, 95e-3, 3, 3, 3]]).T 
-    '''
-
-
     #Define the problem for SALib
     problem = {
         'num_vars': len(default_values),
         'names': default_values.keys(),
-        'bounds': np.array([[0, 0, 0, 3e-7, 0, 0, 0, 95e-7, 25e-4, 0, 92e-4, 5e-7, 0, 0, 0, 4e-4, 0, 0, 5e-4, 0],
-                            [3, 3, 3, 3e-3, 3, 3, 3, 95e-3, 25e-1, 3, 92e-1, 5e-3, 3, 3, 3, 4e-1, 3, 3, 5e-1, 3]]).T 
+        'bounds': np.array([[10, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [60, 4, 25e-2, 6e-15, 2, 10e-2, 92e-2, 92e-2, 6e-16, 2, 0.002, 4e-2, 3, 10e-5]]).T 
     }
 
-    ## number of inputs = 8
+    ## number of inputs = 128
 
     #Generate sobol samples: N*(2D + 2) samples where N: nsamples and D: number of inputs. For sobol analysis N needs to be a power of 2, if not it will give error 
     sobol_samples = sobol.sample(problem, nsamples) 
